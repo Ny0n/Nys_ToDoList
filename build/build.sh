8afddc66d1c4c23983b7c8ec5659e7364356e4d5
Nyøn <flav38320@gmail.com>
2020-10-25 10:53:59 +0100

    Upgrade build management + gitignore

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# first of all, we change the local execution to be the script's folder,
+# so that it works wherever we call it
+cd $(dirname $0)
+
 # addon name
 name='Nys_ToDoList'
-zipname=$name
 
 # here we check if we chose to set a specific tag name
+zipname=$name
 if [ ! -z $1 ]
 then
   zipname="$zipname-$1"
@@ -15,12 +19,9 @@ rm -rf $name *.zip; mkdir $name
 
 # then we create the base folder that has the addon name, and put everyone of the addon's files in it
 shopt -s extglob
-ignore=$(cat zipignore.txt)
-cp -r !($ignore) $name
+ignore=$(cat buildignore.txt)
+cp -r "../"!($ignore) $name
+shopt -u extglob
 
 # then we zip that folder
 C:/Program\ Files/7-Zip/7z.exe a -tzip $zipname.zip $name
-
-# and we end the script by removing the folder, so there is only the zip left
-shopt -u extglob
-rm -rf $name
