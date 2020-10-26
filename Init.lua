6b877e9712040ef559954a2e51d38fdcde48c467
Nyøn <flav38320@gmail.com>
2020-10-26 05:50:33 +0100

    Push 3.0 + Upgrade build management (retail and classic)

--- Init.lua
+++ Init.lua
@@ -1,6 +1,4 @@
---------------------------------------
 -- Namespaces
---------------------------------------
 local _, tdlTable = ...;
 tdlTable.init = {}; -- adds init table to addon namespace
 
@@ -10,110 +8,122 @@ local init = tdlTable.init;
 
 -- Commands:
 init.commands = {
-	[""] = function(...)
+  [""] = function(...)
     itemsFrame:Toggle();
   end,
 
-	["button"] = function(...)
+  ["button"] = function(...)
     itemsFrame:ToggleBtn();
   end,
 
-	["help"] = function(...)
-		config:Print("Type |cff00cc66/tdl command|r to see all the slash commands");
-		config:Print("When adding a new category, the name of the first item is required (there can't be an empty category)");
-		config:Print("To add a new item, type its name in the editbox next to the category name you want to add your item in!");
-		config:Print("You can click on the category names to expand or shrink their content, for more visibility");
-	end,
+  ["help"] = function(...)
+    config:Print("Type |cff00cc66/tdl command|r to see all the slash commands");
+    config:Print("When adding a new category, the name of the first item is required (there can't be an empty category)");
+    config:Print("To add a new item, type its name in the editbox next to the category name you want to add your item in!");
+    config:Print("You can click on the category names to expand or shrink their content, for more visibility");
+  end,
 
-	["command"] = function(...)
-		config:Print("List of slash commands:")
-		config:Print("|cff00cc66/tdl|r - shows items frame");
-		config:Print("|cff00cc66/tdl button|r - shows or hides the toggle button");
-		config:Print("|cff00cc66/tdl help|r - shows help info");
-		config:Print("|cff00cc66/tdl uc|r - unchecks every item");
-		config:Print("|cff00cc66/tdl clear|r - clears the entire list (be careful with that!)");
-	end,
+  ["command"] = function(...)
+    config:Print("List of slash commands:")
+    config:Print("|cff00cc66/tdl|r - shows items frame");
+    config:Print("|cff00cc66/tdl button|r - shows or hides the toggle button");
+    config:Print("|cff00cc66/tdl help|r - shows help info");
+    config:Print("|cff00cc66/tdl uc|r - unchecks every item");
+    config:Print("|cff00cc66/tdl clear|r - clears the entire list");
+  end,
 
-	["uc"] = function(...)
-		itemsFrame:ResetBtns("All");
-	end,
+  ["uc"] = function(...)
+    itemsFrame:ResetBtns("All");
+  end,
 
-	["clear"] = function(...)
-    itemsFrame:ClearAll();
+  ["clear"] = function(...)
+    itemsFrame:ClearTab("All");
+  end,
+
+  ["undo"] = function(...)
+    itemsFrame:UndoRemove();
   end,
 }
 
 -- Command catcher:
 local function HandleSlashCommands(str)
-	local path = init.commands; -- optimise!
+  local path = init.commands; -- optimise!
 
-	if (#str == 0) then
-		-- User just entered "/tdl" with no additional args.
-		path[""]();
-		return;
-	end
+  if (#str == 0) then
+    -- User just entered "/tdl" with no additional args.
+    path[""]();
+    return;
+  end
 
-	local args = {string.split(' ', str)};
+  local args = {string.split(' ', str)};
 
-	local deep = 1;
-	for id, arg in pairs(args) do
-		arg = arg:lower(); -- current arg to low caps
+  local deep = 1;
+  for id, arg in pairs(args) do
+    arg = arg:lower(); -- current arg to low caps
 
-		if (path[arg]) then
-			if (type(path[arg]) == "function") then
-				-- all remaining args passed to our function!
-				path[arg](select(id+1, unpack(args)))
-				return;
-			elseif (type(path[arg]) == "table") then
-				deep = deep+1;
-				path = path[arg]; -- another sub-table found!
+    if (path[arg]) then
+      if (type(path[arg]) == "function") then
+        -- all remaining args passed to our function!
+        path[arg](select(id + 1, unpack(args)))
+        return;
+      elseif (type(path[arg]) == "table") then
+        deep = deep + 1;
+        path = path[arg]; -- another sub-table found!
 
-				if ((select(deep,unpack(args))) == nil) then
-					-- User just entered "/tdl" with no additional args.
-					path[""]();
-					return;
-				end
-			end
-		else
-			-- does not exist!
-			init.commands["help"]();
-			return;
-		end
-	end
+        if ((select(deep, unpack(args))) == nil) then
+          -- User just entered "/tdl" with no additional args.
+          path[""]();
+          return;
+        end
+      end
+    else
+      -- does not exist!
+      init.commands["help"]();
+      return;
+    end
+  end
 end
 
 --Loading the AddOn----------------
 
 -- Initialisation
 function init:Load(self, name)
-	if (name ~= "Nys_ToDoList") then
-		return;
-	end
+  if (name ~= "Nys_ToDoList") then
+    return;
+  end
 
   -- Register new Slash Command!
-	SLASH_ToDoList1 = "/tdl";
-	SlashCmdList.ToDoList = HandleSlashCommands;
+  SLASH_ToDoList1 = "/tdl";
+  SlashCmdList.ToDoList = HandleSlashCommands;
 
-	-- Initializing the saved variable
-	-- since last update, there were a lot more saved variables, and in this one now
-	-- i got rid of all of them and just placed everything in one brand new and alone saved variable,
-	-- this means that to keep everything that was saved, we have to transfer the old variables in the new one,
-	-- the first time and only the first time that we load the addon after the update (this is temporary)
-	if (not ToDoListSV) then
-		ToDoListSV = {
-			checkedButtons = ToDoListSV_checkedButtons or {},
-			itemsList = ToDoListSV_itemsList or { ["Daily"] = {}, ["Weekly"] = {} },
-			autoReset = ToDoListSV_autoReset or { ["Daily"] = config:GetSecondsToReset().daily, ["Weekly"] = config:GetSecondsToReset().weekly },
-			lastLoadedTab = ToDoListSV_lastLoadedTab or "ToDoListUIFrameTab1",
-			closedCategories = {},
-			newCatClosed = true,
-		}
-	end
+  -- Initializing the saved variable
+  if (ToDoListSV == nil) then
+    ToDoListSV = {};
+  end
+
+  if (ToDoListSV.itemsList == nil) then ToDoListSV.itemsList = { ["Daily"] = {}, ["Weekly"] = {} } end
+
+  if (ToDoListSV.weeklyDay == nil) then ToDoListSV.weeklyDay = 4 end
+  if (ToDoListSV.dailyHour == nil) then ToDoListSV.dailyHour = 8 end
+  if (ToDoListSV.autoReset == nil) then ToDoListSV.autoReset = { ["Daily"] = config:GetSecondsToReset().daily, ["Weekly"] = config:GetSecondsToReset().weekly } end
+
+  if (ToDoListSV.newCatClosed == nil) then ToDoListSV.newCatClosed = true end
+  if (ToDoListSV.optionsClosed == nil) then ToDoListSV.optionsClosed = true end
+  if (ToDoListSV.toggleBtnIsShown == nil) then ToDoListSV.toggleBtnIsShown = true end
+  if (ToDoListSV.showChatMessages == nil) then ToDoListSV.showChatMessages = true end
+  if (ToDoListSV.rememberUndo == nil) then ToDoListSV.rememberUndo = true end
+
+  if (ToDoListSV.lastLoadedTab == nil) then ToDoListSV.lastLoadedTab = "ToDoListUIFrameTab1" end
+  if (ToDoListSV.checkedButtons == nil) then ToDoListSV.checkedButtons = {} end
+  if (ToDoListSV.closedCategories == nil) then ToDoListSV.closedCategories = {} end
+  if (not ToDoListSV.rememberUndo) then ToDoListSV.undoTable = nil end
+  if (ToDoListSV.undoTable == nil) then ToDoListSV.undoTable = {} end
+
 
   -- We load the frame
   config:CreateItemsFrame();
 
-	config:Print("ToDoList loaded! type |cff00cc66/tdl help|r for information.");
+  config:Print("ToDoList loaded! type |cff00cc66/tdl help|r for information.");
 end
 
 --Creating the virtual frame to handle the event
--- build/build.sh
+++ build/build.sh
@@ -1,27 +1,82 @@
 #!/bin/bash
 
-# first of all, we change the local execution to be the script's folder,
-# so that it works wherever we call it
-cd $(dirname $0)
+# -- helpers --
+# grep -Po 'match\K.*' file --> returns everything after match for the rest of the line
+# grep -Po 'match\K[^ ]+' file --> returns the first expression after match for the rest of the line
+# interface=${interface//\\/\\\\}
+# interface=${interface//\*/\\*}
+# interface=${interface//\//\\/}
+# interface=${interface//[/\\[}
+# -------------
 
-# addon name
-name='Nys_ToDoList'
-
-# here we check if we chose to set a specific tag name
-zipname=$name
-if [ ! -z $1 ]
-then
-  zipname="$zipname-$1"
-fi
-
-# first we delete any build folder/zip that already exists
-rm -rf $name *.zip; mkdir $name
-
-# then we create the base folder that has the addon name, and put everyone of the addon's files in it
 shopt -s extglob
-ignore=$(cat buildignore.txt)
-cp -r "../"!($ignore) $name
-shopt -u extglob
+buildAddon()
+{
+  # here we get the build version (retail/classic)
+  if [ -z "$1" ]
+  then
+    buildAddon retail
+    buildAddon classic
+    return
+  else
+    version=$1
+  fi
 
-# then we zip that folder
-C:/Program\ Files/7-Zip/7z.exe a -tzip $zipname.zip $name
+  # first, we change some values inside of the toc file depending on the version we're building
+  interfaceCurrentValue=$(grep -Pom1 "$interfacePrefix.*" ../*.toc)
+  interfaceNumber=$(grep -Pom1 "$version \K.*" interfaceversions.txt)
+  sed -i "s/$interfaceCurrentValue/$interfacePrefix $interfaceNumber # _$version\_/" ../*.toc # interface number
+
+  # here we get the zip name that we'll create
+  zipname="$name-$versionValue"
+  if [ "$version" == "classic" ]
+  then
+    zipname="$zipname"_"$version"
+  fi
+
+  # here we delete any build folder/zip that already exists
+  rm -rf $zipname*.zip $zipname*;
+
+  # then we create the base folder that has the addon name, and put everyone of the addon's files in it
+  mkdir $name
+  ignore=$(cat buildignore.txt)
+  cp -r "../"!($ignore) $name
+
+  # then we zip
+  C:/Program\ Files/7-Zip/7z.exe a -tzip -bso0 -bsp0 $zipname.zip $name # zip the folder
+  C:/Program\ Files/7-Zip/7z.exe x -bso0 -bsp0 $zipname.zip -o$zipname # unzip the new zip to have an easy access to the addon, to a subfolder with the same name of the zip so that it's clearer
+  rm -rf $name # then we can delete the original build folder
+}
+
+if [ ! -z "$1" -a "$1" != "--retail" -a "$1" != "--classic" ] # if we misstyped the argument
+then
+  echo "Usage: build.sh [VERSION]"
+  echo -e "\tnothing \tbuilds the addon for retail and classic"
+  echo -e "\t--retail \tbuilds the addon for retail"
+  echo -e "\t--classic \tbuilds the addon for classic"
+else # let's gooo
+  # variables
+  interfacePrefix="## Interface:"
+  versionPrefix="## Version:"
+
+  # first of all, we change the local execution to be the script's folder,
+  # so that it works wherever we call it
+  cd $(dirname $0)
+
+  # we save the WIP toc file
+  cp ../*.toc .
+  # and remove any 'WIP' in the original's name and file
+  tocNewName=$(basename ../*.toc) | sed -e "s/WIP//"
+  mv ../*.toc ../$tocNewName # remove any WIP in the toc's name
+  sed -i "s/WIP//" ../*.toc # remove any WIP in the toc file
+  name=$(basename ../*.toc .toc)
+
+  versionValue=$(grep -Pom1 "$versionPrefix \K.*" ../*.toc)
+
+  buildAddon $(echo $1 | sed -e "s/--//")
+
+  # and we put back the untouched toc file where it belongs
+  rm -f ../*.toc
+  mv *.toc ../
+fi
+shopt -u extglob
