51a9f428cd62492eca6ac36efad3f4d010ea4ca4
Nyøn <flav38320@gmail.com>
2020-10-26 06:04:42 +0100

    Push 4.0 + upgrade build

---
+++ Locales/enUS.lua
@@ -0,0 +1,111 @@
+local addonName = ...;
+
+-- default language
+local L = LibStub("AceLocale-3.0"):NewLocale(addonName, "enUS", true)
+
+-- ============================================ --
+
+-- Chat messages
+L["To add an item to a category, just click the arrow button next to the category name."] = true
+L["\"%s\" added to %s! (%s item)"] = true
+L["\"%s\" added to %s! ('All' tab item)"] = true
+L["\"%s\" removed!"] = true
+L["/tdl"] = true
+L["addon loaded!"] = true
+L["Clear succesful! (%s tab, %i items)"] = true
+L["Clear undo succesful! (%i items added back)"] = true
+L["Everything's done for this week!"] = true
+L["Everything's done for today!"] = true
+L["Go to the addon options in the Blizzard interface panel to customize this."] = true
+L["help"] = true
+L["info"] = true
+L["No item can be daily and weekly!"] = true
+L["No remove/clear to undo!"] = true
+L["Nothing to clear here!"] = true
+L["Nothing to uncheck here!"] = true
+L["Please enter a category name!"] = true
+L["Please enter the name of the item!"] = true
+L["show/hide the list"] = true
+L["shows more information"] = true
+L["The category name cannot be '%s' or '%s', sorry! (there are tabs for that!)"] = true
+L["There can't be an empty category."] = true
+L["There is a minimap button (the default), a bigger TDL button, you can activate the titan panel plugin for this addon, the /tdl command, and you can also bind a key."] = true
+L["This categoty name is too big!"] = true
+L["This item is already here in this category!"] = true
+L["This item name already exists!"] = true
+L["This item name is too big!"] = true
+L["To toggle the list, you have several ways:"] = true
+L["Unchecked %s tab!"] = true
+L["Unchecked everything!"] = true
+L["You can click on the category names to expand or shrink their content."] = true
+L["Nice job, you did everything on the list for this week!"] = true
+
+-- Options frame
+L["Key Binding"] = true
+L["Buttons"] = true
+L["Settings"] = true
+L["Auto Uncheck"] = true
+L["Press a key to bind, ESC to clear the binding or click the button again to cancel"] = true
+L["(independant from profile)"] = true
+L["Activate or desactivate the chat messages"] = true
+L["Are you sure?"] = true
+L["Bind a key to toggle the list"] = true
+L["Choose the day for the weekly reset"] = true
+L["Choose the hour for the daily reset"] = true
+L["Daily reset hour"] = true
+L["General"] = true
+L["Manage general options"] = true
+L["Options"] = true
+L["Remember undos"] = true
+L["Resetting this profile will also clear the list."] = true
+L["Save undos between sessions"] = true
+L["Show chat messages"] = true
+L["Show minimap button"] = true
+L["Show TDL button"] = true
+L["Show the tooltip of the minimap/databroker button"] = true
+L["Show tooltip"] = true
+L["Show/Hide the list"] = true
+L["This action will override your settings, including the list."] = true
+L["Toggles the display of the minimap button"] = true
+L["Toggles the display of the 'To-Do List' button"] = true
+L["WARNING"] = true
+L["Weekly reset day"] = true
+
+-- Random
+L["A simple interface to remember everything you need to do!"] = true
+L["Click"] = true
+L["Ctrl-Click"] = true
+L["Friday"] = true
+L["lock minimap button"] = true
+L["Monday"] = true
+L["Saturday"] = true
+L["Shift-Click"] = true
+L["Show/Hide the To-Do List"] = true
+L["Sunday"] = true
+L["Thursday"] = true
+L["toggle addon options"] = true
+L["toggle the list"] = true
+L["Tuesday"] = true
+L["unlock minimap button"] = true
+L["Wednesday"] = true
+
+-- To-Do List frame
+L["Press enter or click to add the item"] = true
+L["1st item name:"] = true
+L["Add a new category"] = true
+L["Add category"] = true
+L["All"] = true
+L["Category name:"] = true
+L["Change the background opacity"] = true
+L["Change the opacity for texts, buttons and other elements"] = true
+L["Clear tab"] = true
+L["Daily"] = true
+L["Frame content opacity"] = true
+L["Frame opacity"] = true
+L["Frame options"] = true
+L["Open addon options"] = true
+L["Remaining:"] = true
+L["There are no items!"] = true
+L["Uncheck tab"] = true
+L["Undo"] = true
+L["Weekly"] = true
