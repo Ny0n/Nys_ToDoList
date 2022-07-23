895c6903ab95d3c458071999803f8a68d3af344e
Ny0n <flav38320@gmail.com>
2022-07-23 21:47:06 +0200

    Cleanup (.editorconfig, luacheck)

--- Nys_ToDoList/Assets/Scripts/General/tutorialsManager.lua
+++ Nys_ToDoList/Assets/Scripts/General/tutorialsManager.lua
@@ -3,7 +3,6 @@ local addonName, addonTable = ...
 
 -- addonTable aliases
 local core = addonTable.core
-local enums = addonTable.enums
 local utils = addonTable.utils
 local widgets = addonTable.widgets
 local mainFrame = addonTable.mainFrame
@@ -18,62 +17,62 @@ local tutorialFramesTarget = {}
 
 -- default ordered tutorial
 local tutorialOrder = {
-  "TM_introduction_addNewCat",
-  "TM_introduction_addCat",
-  "TM_introduction_addItem",
-  "TM_introduction_accessOptions",
-  "TM_introduction_getMoreInfo",
-  "TM_introduction_editmode",
-  "TM_introduction_editmodeChat",
+	"TM_introduction_addNewCat",
+	"TM_introduction_addCat",
+	"TM_introduction_addItem",
+	"TM_introduction_accessOptions",
+	"TM_introduction_getMoreInfo",
+	"TM_introduction_editmode",
+	"TM_introduction_editmodeChat",
 }
 
 --[[ -- TDLATER new tuto system
 
 -- tutorials_progression how it works:
 -- tutorials_progression = {
---   -- "tutoName" = true/nil,
---   -- "tutoName" = true/nil,
---   -- ...
+-- 	-- "tutoName" = true/nil,
+-- 	-- "tutoName" = true/nil,
+-- 	-- ...
 -- },
 
 -- tutorials (names are unique)
 local tutorials = {
-  ["introduction"] = {
-    IsEnabled = function()
-      return not NysTDL.db.global.tutorials_progression["introduction"]
-    end,
-    tutosOrdered = {
-      "TM_introduction_addNewCat",
-      "TM_introduction_addCat",
-      "TM_introduction_addItem",
-      "TM_introduction_accessOptions",
-      "TM_introduction_getMoreInfo",
-    },
-    progress = 0,
-    OnFinish = function()
-      NysTDL.db.global.tutorials_progression["introduction"] = true
-    end
-  },
-  ["editmode"] = {
-    IsEnabled = function()
-      return NysTDL.db.global.tutorials_progression["introduction"]
-      and not NysTDL.db.global.tutorials_progression["editmode"]
-    end,
-    tutosOrdered = {
-      "TM_editmode_editmodeBtn",
-      "TM_editmode_delete",
-      "TM_editmode_favdesc",
-      "TM_editmode_rename",
-      "TM_editmode_sort",
-      "TM_editmode_resize",
-      "TM_editmode_buttons",
-      "TM_editmode_undo",
-    },
-    progress = 0,
-    OnFinish = function()
-      NysTDL.db.global.tutorials_progression["editmode"] = true
-    end
-  },
+	["introduction"] = {
+		IsEnabled = function()
+			return not NysTDL.db.global.tutorials_progression["introduction"]
+		end,
+		tutosOrdered = {
+			"TM_introduction_addNewCat",
+			"TM_introduction_addCat",
+			"TM_introduction_addItem",
+			"TM_introduction_accessOptions",
+			"TM_introduction_getMoreInfo",
+		},
+		progress = 0,
+		OnFinish = function()
+			NysTDL.db.global.tutorials_progression["introduction"] = true
+		end
+	},
+	["editmode"] = {
+		IsEnabled = function()
+			return NysTDL.db.global.tutorials_progression["introduction"]
+			and not NysTDL.db.global.tutorials_progression["editmode"]
+		end,
+		tutosOrdered = {
+			"TM_editmode_editmodeBtn",
+			"TM_editmode_delete",
+			"TM_editmode_favdesc",
+			"TM_editmode_rename",
+			"TM_editmode_sort",
+			"TM_editmode_resize",
+			"TM_editmode_buttons",
+			"TM_editmode_undo",
+		},
+		progress = 0,
+		OnFinish = function()
+			NysTDL.db.global.tutorials_progression["editmode"] = true
+		end
+	},
 }
 
 ]]
@@ -81,97 +80,97 @@ local tutorials = {
 --/*******************/ FRAMES /*************************/--
 
 function tutorialsManager:CreateTutoFrames()
-  -- POLISH if text is bigger than width, ... by default but not right AND frame strata too high
-  tutorialFrames.TM_introduction_addNewCat = widgets:TutorialFrame("TM_introduction_addNewCat", false, "UP", L["Start by adding a new category!"], 190, 50)
-  tutorialFrames.TM_introduction_addCat = widgets:TutorialFrame("TM_introduction_addCat", true, "UP", L["This will add your category to the current tab"], 240, 50)
-  tutorialFrames.TM_introduction_addItem = widgets:TutorialFrame("TM_introduction_addItem", false, "RIGHT", L["To add new items to existing categories, just right-click the category names"], 220, 50)
-  tutorialFrames.TM_introduction_accessOptions = widgets:TutorialFrame("TM_introduction_accessOptions", false, "DOWN", L["You can access the options from here"], 220, 50)
-  tutorialFrames.TM_introduction_getMoreInfo = widgets:TutorialFrame("TM_introduction_getMoreInfo", false, "LEFT", L["If you're having any problems or you just want more information, you can always click here to print help in the chat!"], 275, 50)
-  tutorialFrames.TM_introduction_editmode = widgets:TutorialFrame("TM_introduction_editmode", false, "DOWN", L["To delete items and do a lot more, you can right-click anywhere on the list or click on this button to toggle the edit mode"], 275, 50)
-  tutorialFrames.TM_introduction_editmodeChat = widgets:TutorialFrame("TM_introduction_editmodeChat", true, "RIGHT", utils:SafeStringFormat(L["Please type %s and read the chat message for more information about this mode"], "\""..core.slashCommand..' '..L["editmode"].."\""), 275, 50)
+	-- POLISH if text is bigger than width, ... by default but not right AND frame strata too high
+	tutorialFrames.TM_introduction_addNewCat = widgets:TutorialFrame("TM_introduction_addNewCat", false, "UP", L["Start by adding a new category!"], 190, 50)
+	tutorialFrames.TM_introduction_addCat = widgets:TutorialFrame("TM_introduction_addCat", true, "UP", L["This will add your category to the current tab"], 240, 50)
+	tutorialFrames.TM_introduction_addItem = widgets:TutorialFrame("TM_introduction_addItem", false, "RIGHT", L["To add new items to existing categories, just right-click the category names"], 220, 50)
+	tutorialFrames.TM_introduction_accessOptions = widgets:TutorialFrame("TM_introduction_accessOptions", false, "DOWN", L["You can access the options from here"], 220, 50)
+	tutorialFrames.TM_introduction_getMoreInfo = widgets:TutorialFrame("TM_introduction_getMoreInfo", false, "LEFT", L["If you're having any problems or you just want more information, you can always click here to print help in the chat!"], 275, 50)
+	tutorialFrames.TM_introduction_editmode = widgets:TutorialFrame("TM_introduction_editmode", false, "DOWN", L["To delete items and do a lot more, you can right-click anywhere on the list or click on this button to toggle the edit mode"], 275, 50)
+	tutorialFrames.TM_introduction_editmodeChat = widgets:TutorialFrame("TM_introduction_editmodeChat", true, "RIGHT", utils:SafeStringFormat(L["Please type %s and read the chat message for more information about this mode"], "\""..core.slashCommand..' '..L["editmode"].."\""), 275, 50)
 
-  -- tutorialFrames.TM_editmode_editmodeBtn = widgets:TutorialFrame("TM_editmode_editmodeBtn", false, "DOWN", L["To delete items and do a lot more, you can right-click anywhere on the list or click on this button to toggle the edit mode"], 275, 50)
-  -- tutorialFrames.TM_editmode_delete = widgets:TutorialFrame("TM_editmode_delete", true, "RIGHT", L["Delete items and categories"], 275, 50)
-  -- tutorialFrames.TM_editmode_favdesc = widgets:TutorialFrame("TM_editmode_favdesc", true, "RIGHT", L["Favorite and add descriptions on items"], 275, 50)
-  -- tutorialFrames.TM_editmode_rename = widgets:TutorialFrame("TM_editmode_rename", false, "UP", L["Rename items and categories"].." ("..L["Double-Click"]..")", 275, 50)
-  -- tutorialFrames.TM_editmode_sort = widgets:TutorialFrame("TM_editmode_sort", false, "DOWN", L["Reorder/Sort the list"].." ("..L["Drag and Drop"]..")", 275, 50)
-  -- tutorialFrames.TM_editmode_resize = widgets:TutorialFrame("TM_editmode_resize", true, "LEFT", L["Resize the list"], 275, 50)
-  -- tutorialFrames.TM_editmode_buttons = widgets:TutorialFrame("TM_editmode_buttons", true, "DOWN", L["Undo what you deleted and access special actions for the tab"], 275, 50)
-  -- tutorialFrames.TM_editmode_undo = widgets:TutorialFrame("TM_editmode_undo", true, "DOWN", L["More specifically you can undo items, categories, and even tab deletions"], 275, 50)
+	-- tutorialFrames.TM_editmode_editmodeBtn = widgets:TutorialFrame("TM_editmode_editmodeBtn", false, "DOWN", L["To delete items and do a lot more, you can right-click anywhere on the list or click on this button to toggle the edit mode"], 275, 50)
+	-- tutorialFrames.TM_editmode_delete = widgets:TutorialFrame("TM_editmode_delete", true, "RIGHT", L["Delete items and categories"], 275, 50)
+	-- tutorialFrames.TM_editmode_favdesc = widgets:TutorialFrame("TM_editmode_favdesc", true, "RIGHT", L["Favorite and add descriptions on items"], 275, 50)
+	-- tutorialFrames.TM_editmode_rename = widgets:TutorialFrame("TM_editmode_rename", false, "UP", L["Rename items and categories"].." ("..L["Double-Click"]..")", 275, 50)
+	-- tutorialFrames.TM_editmode_sort = widgets:TutorialFrame("TM_editmode_sort", false, "DOWN", L["Reorder/Sort the list"].." ("..L["Drag and Drop"]..")", 275, 50)
+	-- tutorialFrames.TM_editmode_resize = widgets:TutorialFrame("TM_editmode_resize", true, "LEFT", L["Resize the list"], 275, 50)
+	-- tutorialFrames.TM_editmode_buttons = widgets:TutorialFrame("TM_editmode_buttons", true, "DOWN", L["Undo what you deleted and access special actions for the tab"], 275, 50)
+	-- tutorialFrames.TM_editmode_undo = widgets:TutorialFrame("TM_editmode_undo", true, "DOWN", L["More specifically you can undo items, categories, and even tab deletions"], 275, 50)
 end
 
 function tutorialsManager:SetPoint(tutoName, point, relativeTo, relativePoint, ofsx, ofsy)
-  -- sets the points and target frame of a given tutorial
-  if utils:HasValue(tutorialOrder, tutoName) then
-    tutorialFramesTarget[tutoName] = relativeTo
-    tutorialFrames[tutoName]:ClearAllPoints()
-    tutorialFrames[tutoName]:SetPoint(point, relativeTo, relativePoint, ofsx, ofsy)
-  end
+	-- sets the points and target frame of a given tutorial
+	if utils:HasValue(tutorialOrder, tutoName) then
+		tutorialFramesTarget[tutoName] = relativeTo
+		tutorialFrames[tutoName]:ClearAllPoints()
+		tutorialFrames[tutoName]:SetPoint(point, relativeTo, relativePoint, ofsx, ofsy)
+	end
 end
 
 function tutorialsManager:SetFramesScale(scale)
-  for _, v in pairs(tutorialFrames) do
-    v:SetScale(scale)
-  end
+	for _, v in pairs(tutorialFrames) do
+		v:SetScale(scale)
+	end
 end
 
 function tutorialsManager:UpdateFramesVisibility()
-  -- here we manage the visibility of the tutorial frames, showing them if their corresponding frames are shown,
-  -- their tuto has not been completed (false) and the previous one is true.
-  -- this is called by the OnUpdate event of the tdlFrame
+	-- here we manage the visibility of the tutorial frames, showing them if their corresponding frames are shown,
+	-- their tuto has not been completed (false) and the previous one is true.
+	-- this is called by the OnUpdate event of the tdlFrame
 
-  -- for _,tuto in ipairs(tutorials) do
-  --   if tuto:IsEnabled() then
-  --     TDLATER
-  --   end
-  -- end
+--   for _,tuto in ipairs(tutorials) do
+--     if tuto:IsEnabled() then
+-- 		-- TDLATER
+--     end
+--   end
 
-  if NysTDL.db.global.tuto_progression < #tutorialOrder then
-    for i, v in pairs(tutorialOrder) do
-      local r = false
-      if NysTDL.db.global.tuto_progression < i then -- if the current loop tutorial has not already been done
-        if NysTDL.db.global.tuto_progression == i-1 then -- and the previous one has been done
-          if tutorialFramesTarget[v] ~= nil and tutorialFramesTarget[v]:IsVisible() then -- and his corresponding target frame is currently visible
-            r = true -- then we can show the tutorial frame
-          end
-        end
-      end
-      tutorialFrames[v]:SetShown(r)
-    end
-  elseif NysTDL.db.global.tuto_progression == #tutorialOrder then -- we completed the last tutorial
-    tutorialFrames[tutorialOrder[#tutorialOrder]]:SetShown(false) -- we don't need to do the big loop above, we just need to hide the last tutorial frame (it's just optimization)
-    tutorialsManager:Next() -- and we also add a step of progression, just so that we never enter this 'if' again. (optimization too :D)
-    -- mainFrame:Event_TDLFrame_OnVisibilityUpdate() -- and finally, we reset the menu openings of the list at the end of the tutorial, for more visibility
-  end
+	if NysTDL.db.global.tuto_progression < #tutorialOrder then
+		for i, v in pairs(tutorialOrder) do
+			local isShown = false
+			if NysTDL.db.global.tuto_progression < i then -- if the current loop tutorial has not already been done
+				if NysTDL.db.global.tuto_progression == i-1 then -- and the previous one has been done
+					if tutorialFramesTarget[v] ~= nil and tutorialFramesTarget[v]:IsVisible() then -- and his corresponding target frame is currently visible
+						isShown = true -- then we can show the tutorial frame
+					end
+				end
+			end
+			tutorialFrames[v]:SetShown(isShown)
+		end
+	elseif NysTDL.db.global.tuto_progression == #tutorialOrder then -- we completed the last tutorial
+		tutorialFrames[tutorialOrder[#tutorialOrder]]:SetShown(false) -- we don't need to do the big loop above, we just need to hide the last tutorial frame (it's just optimization)
+		tutorialsManager:Next() -- and we also add a step of progression, just so that we never enter this 'if' again. (optimization too :D)
+		-- mainFrame:Event_TDLFrame_OnVisibilityUpdate() -- and finally, we reset the menu openings of the list at the end of the tutorial, for more visibility
+	end
 end
 
 --/*******************/ MANAGMENT /*************************/--
 
 function tutorialsManager:Validate(tuto_name)
-  -- completes the "tuto_name" tutorial, only if it was active
-  local i = utils:GetKeyFromValue(tutorialOrder, tuto_name)
-  if NysTDL.db.global.tuto_progression < i then
-    if NysTDL.db.global.tuto_progression == i-1 then
-      tutorialsManager:Next() -- we validate the tutorial by going to the next one
-      return true
-    end
-  end
+	-- completes the "tuto_name" tutorial, only if it was active
+	local i = utils:GetKeyFromValue(tutorialOrder, tuto_name)
+	if NysTDL.db.global.tuto_progression < i then
+		if NysTDL.db.global.tuto_progression == i-1 then
+			tutorialsManager:Next() -- we validate the tutorial by going to the next one
+			return true
+		end
+	end
 end
 
 function tutorialsManager:Next()
-  NysTDL.db.global.tuto_progression = NysTDL.db.global.tuto_progression + 1
+	NysTDL.db.global.tuto_progression = NysTDL.db.global.tuto_progression + 1
 end
 
 function tutorialsManager:Previous()
-  NysTDL.db.global.tuto_progression = NysTDL.db.global.tuto_progression - 1
-  if NysTDL.db.global.tuto_progression < 0 then
-    NysTDL.db.global.tuto_progression = 0
-  end
+	NysTDL.db.global.tuto_progression = NysTDL.db.global.tuto_progression - 1
+	if NysTDL.db.global.tuto_progression < 0 then
+		NysTDL.db.global.tuto_progression = 0
+	end
 end
 
 function tutorialsManager:Reset()
-  NysTDL.db.global.tuto_progression = 0
-  -- wipe(NysTDL.db.global.tutorials_progression) TDLATER
-  mainFrame:Event_TDLFrame_OnVisibilityUpdate()
-  mainFrame:GetFrame().ScrollFrame:SetVerticalScroll(0)
+	NysTDL.db.global.tuto_progression = 0
+	-- wipe(NysTDL.db.global.tutorials_progression) TDLATER
+	mainFrame:Event_TDLFrame_OnVisibilityUpdate()
+	mainFrame:GetFrame().ScrollFrame:SetVerticalScroll(0)
 end
--- Nys_ToDoList/Assets/Scripts/General/utils.lua
+++ Nys_ToDoList/Assets/Scripts/General/utils.lua
@@ -78,11 +78,11 @@ function utils:IsVersionOlderThan(v1, v2)
 end
 
 function utils:Clamp(number, min, max)
-  return math.min(math.max(number, min), max)
+	return math.min(math.max(number, min), max)
 end
 
 function utils:RGBToHex(rgb)
-  -- thanks to marceloCodget/gist:3862929 on GitHub
+	-- from marceloCodget/gist:3862929 on GitHub
 
 	local hexadecimal = ""
 
@@ -97,7 +97,6 @@ function utils:RGBToHex(rgb)
 
 		if string.len(hex) == 0 then
 			hex = '00'
-
 		elseif string.len(hex) == 1 then
 			hex = '0'..hex
 		end
@@ -110,108 +109,109 @@ end
 
 local T_ThemeDownTo01 = {}
 function utils:ThemeDownTo01(theme)
-  local r, g, b = unpack(theme)
+	local r, g, b = unpack(theme)
 
-  wipe(T_ThemeDownTo01)
-  table.insert(T_ThemeDownTo01, r/255)
-  table.insert(T_ThemeDownTo01, g/255)
-  table.insert(T_ThemeDownTo01, b/255)
+	wipe(T_ThemeDownTo01)
+	table.insert(T_ThemeDownTo01, r/255)
+	table.insert(T_ThemeDownTo01, g/255)
+	table.insert(T_ThemeDownTo01, b/255)
 
-  return T_ThemeDownTo01
+	return T_ThemeDownTo01
 end
 
 local T_DimTheme = {}
 function utils:DimTheme(theme, dim)
-  local r, g, b = unpack(theme)
+	local r, g, b = unpack(theme)
 
-  wipe(T_DimTheme)
-  table.insert(T_DimTheme, r*dim)
-  table.insert(T_DimTheme, g*dim)
-  table.insert(T_DimTheme, b*dim)
+	wipe(T_DimTheme)
+	table.insert(T_DimTheme, r*dim)
+	table.insert(T_DimTheme, g*dim)
+	table.insert(T_DimTheme, b*dim)
 
-  return T_DimTheme
+	return T_DimTheme
 end
 
 function utils:Deepcopy(orig, copies)
-  copies = copies or {}
-  local orig_type = type(orig)
-  local copy
-  if orig_type == 'table' then
-    if copies[orig] then
-      copy = copies[orig]
-    else
-      copy = {}
-      copies[orig] = copy
-      for orig_key, orig_value in next, orig, nil do
-        copy[self:Deepcopy(orig_key, copies)] = self:Deepcopy(orig_value, copies)
-      end
-      setmetatable(copy, self:Deepcopy(getmetatable(orig), copies))
-    end
-  else -- number, string, boolean, etc
-    copy = orig
-  end
-  return copy
+	copies = copies or {}
+	local orig_type = type(orig)
+	local copy
+	if orig_type == 'table' then
+		if copies[orig] then
+			copy = copies[orig]
+		else
+			copy = {}
+			copies[orig] = copy
+			for orig_key, orig_value in next, orig, nil do
+				copy[utils:Deepcopy(orig_key, copies)] = utils:Deepcopy(orig_value, copies)
+			end
+			setmetatable(copy, utils:Deepcopy(getmetatable(orig), copies))
+		end
+	else -- number, string, boolean, etc
+		copy = orig
+	end
+	return copy
 end
 
 function utils:SafeStringFormat(str, ...)
-  -- safe format, in case there is an error in the localization (happened once)
-  -- (we check if there are the necessary %x in the string, corresponding to the content of ...)
-  -- only accepting %i and %s, it's enough for my use
-  local dup = str
-  for i=1, select("#", ...) do
-    local var = select(i, ...)
-    if var then
-      local toMatch = (type(var) == "number") and "%%i" or "%%s"
-      if (string.find(dup, toMatch)) then
-        dup = string.gsub(dup, toMatch, "", 1)
-      else
-        return str.." "..enums.translationErrMsg
-      end
-    end
-  end
-  return str:format(...) -- it should be good
+	-- safe format, in case there is an error in the localization (happened once)
+	-- (we check if there are the necessary %x in the string, corresponding to the content of ...)
+	-- only accepting %i and %s, it's enough for my use
+	local dup = str
+	for i=1, select("#", ...) do
+		local var = select(i, ...)
+		if var then
+			local toMatch = (type(var) == "number") and "%%i" or "%%s"
+			if (string.find(dup, toMatch)) then
+				dup = string.gsub(dup, toMatch, "", 1)
+			else
+				return str.." "..enums.translationErrMsg
+			end
+		end
+	end
+	return str:format(...) -- it should be good
 end
 
 function utils:HasHyperlink(s)
-  if s ~= nil then
-    -- a hyperlink pattern has at least one '|H' and two '|h', so this is the closest test i can think of
-    if (select(2, string.gsub(s, "|H", "")) >= 1) and (select(2, string.gsub(s, "|h", "")) >= 2) then
-      return true
-    end
-  end
-  return false
+	if s ~= nil then
+		-- a hyperlink pattern has at least one '|H' and two '|h', so this is the closest test i can think of
+		-- TDLATER replace with a regex match
+		if (select(2, string.gsub(s, "|H", "")) >= 1) and (select(2, string.gsub(s, "|h", "")) >= 2) then
+			return true
+		end
+	end
+	return false
 end
 
 function utils:HasValue(tbl, value)
-  local isPresent, key = false, 0
-  if type(tbl) == "table" then -- just in case
-	  for k, v in pairs(tbl) do
-	    if v == value then
-	      isPresent = true
-	      key = k
-	      break
-	    end
-	  end
-  end
-  return isPresent, key
+	local isPresent, key = false, 0
+	if type(tbl) == "table" then -- just in case
+		for k, v in pairs(tbl) do
+			if v == value then
+				isPresent = true
+				key = k
+				break
+			end
+		end
+	end
+	return isPresent, key
 end
 
 function utils:HasKey(tbl, key)
-  if type(tbl) == "table" then -- just in case
-	  for k in pairs(tbl) do
-	    if (k == key) then
-	      return true
-	    end
-	  end
-  end
-  return false
+	if type(tbl) == "table" then -- just in case
+		for k in pairs(tbl) do
+			if k == key then
+				return true
+			end
+		end
+	end
+	return false
 end
 
 function utils:GetKeyFromValue(tabSource, value)
-  for k, v in pairs(tabSource) do
-    if v == value then return k end
-  end
-  return nil
+	for k, v in pairs(tabSource) do
+		if v == value then return k end
+	end
+	return nil
 end
 
 function utils:ColorText(colorTable, text)
